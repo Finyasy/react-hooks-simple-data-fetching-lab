0eef855b583c3fbe8736cebc21ffed3cbb62636c
Bryan Bosire <bryanbosire@gmail.com>
2022-07-20 02:36:53 +0300

    commit tree 3d266c094d27d5b2de7c37292960bba7ff9dc2a5

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,30 @@
 // create your App component here
+import React,{useState,useEffect} from "react";
+
+function App(){
+    const [images,setImage] = useState({
+        message: "",
+        status: "",
+    });
+    const [isLoaded,setIsLoaded] = useState(false);
+
+    useEffect(()=>{
+        fetch("https://dog.ceo/api/breeds/image/random")
+        .then(res=>res.json())
+        .then((data)=>{
+            setImage(data);
+            setIsLoaded(true);
+        })
+    },[]);
+    if (!isLoaded) {
+        return <p>Loading...</p>;
+    }
+
+    return (
+        <img src={images.message} alt="A Random Dog"></img>
+    )
+
+
+}
+
+export default App;
